913f042fb2880d3619fd8f03c21353cc6cd977fb
Bader Kevin Brian <kevin.bader96@gmail.com>
2023-03-22 14:08:36 +0200

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+Python Discord bot for using OpenAI's language model GPT-4
+
+## Installation:
+
+### To use this bot, you need 3 things
+
+1. A discord application and bot user
+2. An OpenAI account with a set up payment plan
+3. Access to the gpt-4 model
+
+### Install the required packages
+
+- openai
+- discord
+
+### Create config.ini using config.example.ini and change the following values:
+
+1. **YOUR_DISCORD_SERVER_ID_HERE**
+
+2. **YOUR_DISCORD_BOT_API_KEY_HERE**
+
+3. **YOUR_OPENAI_ORGANIZATION_ID_HERE**
+
+4. **YOUR_OPENAI_API_KEY_HERE**
+
+5. You can now set your history length, I recommend no more than 100. You can also change the bot's identity and behavior by changing the system_message.
+
+Now you can invite your bot to your server. It needs message and editing permissions.
+Then run main.py.
+
+## Usage:
+
+- Allow the bot to use any channels you want it to use. It can naturally talk and remember. You do not need to use a command.
---
+++ main.py
@@ -0,0 +1,92 @@
+import discord
+import openai
+from configparser import ConfigParser
+from discord import app_commands
+
+config_file = "config.ini"
+config = ConfigParser(interpolation=None)
+config.read(config_file)
+
+SERVER_ID = config["discord"]["server_id"]
+DISCORD_API_KEY = config["discord"][str("api_key")]
+OPENAI_ORG = config["openai"][str("organization")]
+OPENAI_API_KEY = config["openai"][str("api_key")]
+
+GUILD = discord.Object(id=SERVER_ID)
+
+SYSTEM_MESSAGE = config["bot"]["system_message"]
+HISTORY_LENGTH = config["bot"]["history_length"]
+
+
+def trim_conversation_history(history, max_length=int(HISTORY_LENGTH)):
+    if len(history) > max_length:
+        history = history[-max_length:]
+    return history
+
+
+class Client(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+        self.conversation_history = []
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=GUILD)
+        await self.tree.sync(guild=GUILD)
+
+    async def on_message(self, message):
+        author = message.author
+
+        if message.author == self.user:
+            return
+
+        input_content = message.content
+        print(f"{message.author}: {input_content}")
+
+        self.conversation_history.append({"role": "system", "content": f"The user is {author.display_name}. {SYSTEM_MESSAGE}"})
+        self.conversation_history.append({"role": "user", "content": input_content})
+        self.conversation_history = trim_conversation_history(self.conversation_history)
+
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=self.conversation_history
+            )
+
+            assistant_response = response["choices"][0]["message"]["content"]
+            self.conversation_history.append({"role": "assistant", "content": assistant_response})
+            self.conversation_history = trim_conversation_history(self.conversation_history)
+
+        except AttributeError:
+            assistant_response = "It looks like you might have to update your openai package. You can do that with ```pip install --upgrade openai```"
+        except ImportError:
+            assistant_response = "You might not have all required packages installed. Make sure you install the openai and discord package"
+        except openai.error.AuthenticationError:
+            assistant_response = "It looks like you don't have access to the gpt-4 model. Please make sure you have been invited by openai and double check your openai API key and organization ID"
+        except openai.error.RateLimitError:
+            assistant_response = "Your rate has been limited. This might be because of too many requests or because your rate limit has been reached."
+        except openai.error.Timeout:
+            assistant_response = "My response is taking too long and I have received a timeout error."
+        except openai.error.APIConnectionError:
+            assistant_response = "I can't connect to the OpenAI servers at the moment. Please try again later!"
+
+        if assistant_response is not None:
+            parts = [assistant_response[i:i + 2000] for i in range(0, len(assistant_response), 2000)]
+            for index, part in enumerate(parts):
+                try:
+                    print(f"GPT4: {part}")
+                    await message.channel.send(part)
+                except discord.errors.Forbidden:
+                    print("GPT4: I am not able to send a message. Do I have the correct permissions on your server?")
+
+
+gpt4_intents = discord.Intents.default()
+gpt4_intents.messages = True
+gpt4_intents.message_content = True
+client = Client(intents=gpt4_intents)
+
+openai.organization = OPENAI_ORG
+openai.api_key = OPENAI_API_KEY
+openai.Model.list()
+
+client.run(DISCORD_API_KEY)
